0d65205ed941d928b710fb3b65b771f93c1c3042
kowalevski <me@kowalevski.com>
2021-09-09 23:35:24 +0200

    removed unused packages and polished layout of images inside posts

--- components/MDXLayoutRenderer.tsx
+++ components/MDXLayoutRenderer.tsx
@@ -8,7 +8,9 @@ import { Pre } from "./Pre";
 import  { DEFAULT_LAYOUT } from '../lib/constants'
 
 export const MDXComponents = {
-  Image: NextImage,
+  Image: (props: any) => <div style={{ maxWidth: '700px', margin: '0 auto 1.5em auto' }}>
+    <NextImage {...props} layout="responsive" />
+  </div>,
   a: CustomLink,
   pre: Pre,
   wrapper: ({ components, layout, ...rest }: any) => {
